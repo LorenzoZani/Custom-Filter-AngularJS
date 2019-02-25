354775ef89d29ac59645dbe938c0f642e7f64380
Lorenzo <lorenzozani1995@gmail.com>
2019-02-25 11:04:04 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,91 @@
+var app = angular.module("myApp", []);
+
+app.controller("MainCtrl", [
+  "$scope",
+  function($scope) {
+    $scope.search = "";
+
+    $scope.options = [
+      {
+        value: "name",
+        key: "Name"
+      },
+      {
+        value: "lastName",
+        key: "Last Name"
+      },
+      {
+        value: "role",
+        key: "Role"
+      }
+    ];
+
+    $scope.filter = $scope.options[0];
+
+    $scope.getFilter = function() {
+      var filter = {};
+      console.log($scope.filter.value);
+
+      console.log(filter);
+
+      filter[$scope.filter.value] = $scope.query;
+      console.log(filter);
+
+      return filter;
+    };
+
+    $scope.people = [
+      {
+        name: "Lorenzo",
+        lastName: "Zani",
+        role: "FrontEnd Developer"
+      },
+      {
+        name: "Alice",
+        lastName: "Cecchetti",
+        role: "FrontEnd Developer"
+      },
+      {
+        name: "Alessandro",
+        lastName: "Oliva",
+        role: "BackEnd Developer"
+      },
+      {
+        name: "Mattia",
+        lastName: "Cutrupi",
+        role: "ServiceNow"
+      },
+      {
+        name: "Mario",
+        lastName: "Rossi",
+        role: " HelpDesk"
+      }
+    ];
+  }
+]);
+
+app.component("card", {
+  replace: "AECM",
+  templateUrl: "card.html",
+  bindings: {
+    people: "@",
+    query: "<",
+    options: "<",
+    filter: "<",
+    getFilter: "&"
+  },
+  controller: "MainCtrl"
+});
+
+app.component("filter", {
+  replace: "AECM",
+  templateUrl: "filter.html",
+  bindings: {
+    people: "@",
+    query: "=",
+    options: "=",
+    filter: "=",
+    getFilter: "&"
+  },
+  controller: "MainCtrl"
+});
